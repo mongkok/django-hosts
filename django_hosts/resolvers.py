ab56b2f591d46adfc845d33d4be1eb3cd847d7a6
mongkok <domake.io@gmail.com>
2019-12-06 17:21:38 +0700

    Fixed lru_cache import

--- django_hosts/resolvers.py
+++ django_hosts/resolvers.py
@@ -6,6 +6,7 @@ scheme, hostname and port you'll need to use the ``reverse`` and
 import re
 
 from importlib import import_module
+from functools import lru_cache
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
@@ -13,7 +14,6 @@ from django.core.signals import setting_changed
 from django.urls import NoReverseMatch, reverse as reverse_path
 from django.utils.encoding import iri_to_uri
 from django.utils.functional import lazy
-from django.utils.lru_cache import lru_cache
 from django.utils.regex_helper import normalize
 
 from .defaults import host as host_cls
